63cbbd07478f1e451d327e4866fd378ca23abd36
Siddhesh Shivgan <49452754+siddheshshivgan@users.noreply.github.com>
2024-07-01 22:10:35 +0530

    Update sip_mailer.py

--- sip_mailer.py
+++ sip_mailer.py
@@ -85,7 +85,7 @@ accounts = [
 ]
 
 # Navigate to the login page
-driver.get(str(os.getenv('PARTNER_DESK')))
+driver.get(os.getenv('PARTNER_DESK'))
 
 for acc in accounts:
     # Locate the username and password fields and enter the login details
